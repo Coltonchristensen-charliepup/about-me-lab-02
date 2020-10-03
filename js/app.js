21ed7a3b0820ed153cf0f0f1ba74784f8a355d1a
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-10-02 21:09:43 -0700

    show updates to content

--- js/app.js
+++ js/app.js
@@ -1,36 +1,65 @@
 'use strict';
-// Colton - Question 1!:
-var name = prompt('What is your name?');
-console.log('Hi ' + name);
+function validUserInput(value){
+  //var valueLowerCase = value.toLowerCase();
+  return  (yesValue(value) || noValue(value)); 
+}
+function yesValue(value){
+  var valueLowerCase = value.toLowerCase();
+  return (valueLowerCase === 'yes' || valueLowerCase === 'y');
+}
+
+function noValue(value){
+  var valueLowerCase = value.toLowerCase();
+  return (valueLowerCase === 'no' || valueLowerCase === 'n');
+}
+
+function getUserInput(question){
+  var answer = prompt(question);
+  if (validUserInput(answer)){
+    return answer;
+  }
+  var invalidAnswer = 'I only accept yes/no or y/n values.'
+  alert(invalidAnswer);
+  return getUserInput(question);
+}
+
+
+var myName = getUserInput('Is my name Colton?');
+console.log('myName', myName);
+if(yesValue(myName)){
+//console.log('Hi ' + myName);
+alert('Yes, that is my name');
+}
 
 // Colton - Question 2!:
-var homeLocation = prompt('Am i from washington');
-var normalizedHomeLocation = homeLocation.toLocaleLowerCase();
-if (homeLocation === 'yes' || homeLocation === 'y') {
-  //console.log('Yes! I live in Seattle')
-  alert('Yes! I live in Seattle');
+var homeLocation = getUserInput('Am i from washington');
+console.log('homeLocation', homeLocation);
+if (yesValue(homeLocation)){
+alert('Yes! I live in Washington');
+//console.log('Yes! I live in Washington')
 }
-// console.log('Nice, ' + homeLocation + ' is great!');
-// alert('It is nice to meet you ' + username + '. ' + homeLocation+ ' is a great place to hail from!');
+
 
 // Colton - Question 3!:
-var favoriteBeer = prompt('Do I like hazy beers?');
-var normalizedFavoritebeer = favoriteBeer.toLocaleLowerCase();
-if(favoriteBeer === 'yes' || favoriteBeer === 'y')
+var favoriteBeer = getUserInput('Do I like hazy beers?');
+console.log('favoriteBeer',favoriteBeer);
+if(yesValue(favoriteBeer)){
 //console.log('Yes! I like hazy beers');
 alert('Yes! I like hazy beers');
-
+}
 // Colton - Question 4!:
-var animalName = prompt('Is my dogs name Chanko?');
-var normalizAnimalName = animalName.toLocaleLowerCase();
-if(animalName === 'no' || animalName === 'n');
+var animalName = getUserInput('Is my dogs name Chanko?');
+console.log('animalName', animalName);
+if (noValue(animalName)){
 //console.log('No! That is not his name!');
 alert('No! That is not his name!');
-//alert('It is Nice to meet you ' + name + ', ' + homeLocation + ' Is a great place to hail from! ' + favoriteBeer + ' is a great choice' + ',' + ' i hope ' + animalName + ' is well behaved!'); 
+}
+
 
 // Colton - Question 5!:
-var dragRacing = prompt('Do I drag race?');
-var normalizedDragRace = dragRace.toLocaleLowerCase();
-if(dragRace === 'Yes' || dragRace === 'y');
+var dragRace = getUserInput('Do I drag race?');
+console.log('dragRace', dragRace);
+if (yesValue(dragRace)){
 //console.log('Yes! I love drag racing!');
 alert('Yes! I love drag racing!');
+}
