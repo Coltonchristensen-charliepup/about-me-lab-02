ac82fa3f46aef32953bbd4977d50dc0e2f467c4e
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-10-03 17:52:56 -0700

    show added questions with loops, along with css to accomidate for styling

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 function validUserInput(value){
   //var valueLowerCase = value.toLowerCase();
-  return (yesValue(value) || noValue(value)); 
+  return (yesValue(value) || noValue(value));
 }
 function yesValue(value){
   var valueLowerCase = value.toLowerCase();
@@ -19,13 +19,13 @@ function getUserInput(question){
   if (validUserInput(answer)){
     return answer;
   }
-  var invalidAnswer = 'I only accept yes/no or y/n values.';
-  alert(invalidAnswer);
+  var invalidAnswer = 'I only accept yes/no or y/n values';
+  alert(invalidAnswer);3
   return getUserInput(question);
 }
 
-
-
+var userName = prompt('What is your name?!');
+//console.log('userName: ', userName);
 var myName = getUserInput('Is my name Colton?');
 //console.log('myName', myName);
 if(yesValue(myName)){
@@ -65,3 +65,59 @@ if (yesValue(dragRace)){
 //console.log('Yes! I love drag racing!');
 alert('Yes! I love drag racing!');
 }
+
+
+// colton - Question 6!:
+
+console.log(numberOfCounties);
+var countyGuesses = 0;
+
+while (countyGuesses < 6 ){
+ // var numberOfCounties = getUserInput('How many counties have i lived in? Answer the correct number.');
+
+  //prompt('How many counties have i lived in? Answer the correct number.');
+  var numberOfCounties = prompt('How many counties have i lived in? Answer the correct number.');
+  if (isNaN(numberOfCounties)){
+    alert('enter a number!');
+    continue
+  }
+  if (numberOfCounties > 3){
+    alert('try again, ' + userName + ', try agian') 
+  } else if(numberOfCounties < 3){
+    alert('try again, ' + userName + ', try again')
+  } else if (numberOfCounties === '3'){
+    correctAnswer++;
+    break;
+  }
+  countyGuesses++;
+}
+
+// colton- Question 7!:
+
+console.log(numberOfTurbos);
+var turboGuesses = 0;
+
+while(turboGuesses < 12 ){
+  
+  var numberOfTurbos = prompt('How many turbos does my truck have? Answer the correct number');
+  if (isNaN(numberOfTurbos)){
+    alert('enter a number');
+    continue
+  }
+  if (numberOfTurbos > 2){
+    alert('try again, ' + userName + ', try again')
+  }else if(numberOfTurbos < 2){
+    alert('try again, ' + userName + ', try again')
+  }else if(numberOfTurbos === '2'){
+    correctAnswer++;
+    break;
+  }
+}
+
+
+alert('Thanks for playing ' + userName + '!');
+
+// Thanks for playing userName!
+//alert(`Hi ${userName}, this is template literal!`);
+
+ 
