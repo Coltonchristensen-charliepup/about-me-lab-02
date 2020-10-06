6c79b881d3ae9483bf0dfa71e9ff0ddd5bc077e9
coltonchristensen-charliepup <colton.james.christensen@gmail.com>
2020-10-05 19:32:01 -0700

    show you changes to proper syntax

--- js/app.js
+++ js/app.js
@@ -1,26 +1,26 @@
 'use strict';
 
-function validUserInput(value){
+function validUserInput(value) {
   //var valueLowerCase = value.toLowerCase();
   return (yesValue(value) || noValue(value));
 }
-function yesValue(value){
+function yesValue(value) {
   var valueLowerCase = value.toLowerCase();
   return (valueLowerCase === 'yes' || valueLowerCase === 'y');
 }
 
-function noValue(value){
+function noValue(value) {
   var valueLowerCase = value.toLowerCase();
   return (valueLowerCase === 'no' || valueLowerCase === 'n');
 }
 
-function getUserInput(question){
+function getUserInput(question) {
   var answer = prompt(question);
-  if (validUserInput(answer)){
+  if (validUserInput(answer)) {
     return answer;
   }
   var invalidAnswer = 'I only accept yes/no or y/n values';
-  alert(invalidAnswer);3
+  alert(invalidAnswer); 3
   return getUserInput(question);
 }
 
@@ -28,92 +28,91 @@ var userName = prompt('What is your name?!');
 //console.log('userName: ', userName);
 var myName = getUserInput('Is my name Colton?');
 //console.log('myName', myName);
-if(yesValue(myName)){
-//console.log('Hi ' + myName);
-alert('Yes, that is my name');
+if (yesValue(myName)) {
+  //console.log('Hi ' + myName);
+  alert('Yes, that is my name');
 }
 
 // Colton - Question 2!:
 var homeLocation = getUserInput('Am i from washington');
 //console.log('homeLocation', homeLocation);
-if (yesValue(homeLocation)){
-alert('Yes! I live in Washington');
-//console.log('Yes! I live in Washington')
+if (yesValue(homeLocation)) {
+  alert('Yes! I live in Washington');
+  //console.log('Yes! I live in Washington')
 }
 
 
 // Colton - Question 3!:
 var favoriteBeer = getUserInput('Do I like hazy beers?');
 //console.log('favoriteBeer',favoriteBeer);
-if(yesValue(favoriteBeer)){
-//console.log('Yes! I like hazy beers');
-alert('Yes! I like hazy beers');
+if (yesValue(favoriteBeer)) {
+  //console.log('Yes! I like hazy beers');
+  alert('Yes! I like hazy beers');
 }
 // Colton - Question 4!:
 var animalName = getUserInput('Is my dogs name Chanko?');
 //console.log('animalName', animalName);
-if (noValue(animalName)){
-//console.log('No! That is not his name!');
-alert('No! That is not his name!');
+if (noValue(animalName)) {
+  //console.log('No! That is not his name!');
+  alert('No! That is not his name!');
 }
 
 
 // Colton - Question 5!:
 var dragRace = getUserInput('Do I drag race?');
 //console.log('dragRace', dragRace);
-if (yesValue(dragRace)){
-//console.log('Yes! I love drag racing!');
-alert('Yes! I love drag racing!');
+if (yesValue(dragRace)) {
+  //console.log('Yes! I love drag racing!');
+  alert('Yes! I love drag racing!');
 }
 
 
 // colton - Question 6!:
 
-console.log(numberOfCounties);
 var countyGuesses = 0;
+console.log(numberOfCounties);
 
-while (countyGuesses < 6 ){
- // var numberOfCounties = getUserInput('How many counties have i lived in? Answer the correct number.');
+while (countyGuesses < 6) {
+  // var numberOfCounties = getUserInput('How many counties have i lived in? Answer the correct number.');
 
   //prompt('How many counties have i lived in? Answer the correct number.');
   var numberOfCounties = prompt('How many counties have i lived in? Answer the correct number.');
-  if (isNaN(numberOfCounties)){
+  if (isNaN(numberOfCounties)) {
     alert('enter a number!');
     continue
   }
-  if (numberOfCounties > 3){
-    alert('try again, ' + userName + ', try agian') 
-  } else if(numberOfCounties < 3){
+  if (numberOfCounties > 3) {
+    alert('try again, ' + userName + ', try agian')
+  } else if (numberOfCounties < 3) {
     alert('try again, ' + userName + ', try again')
-  } else if (numberOfCounties === 3){
-    correctAnswer++;
-    break;
+  } else if (numberOfCounties === '3') {
+
+    countyGuesses = 6;
   }
   countyGuesses++;
 }
 
 // colton- Question 7!:
 
-console.log(numberOfDogsInMyHouse);
 var attempts = 0;
+console.log(numberOfDogsInMyHouse);
 
-while(attempts < 6){
+while (attempts < 6) {
   attempts++;
   var numberOfDogsInMyHouse = prompt('How many dogs are in my house? Enter the correct number!').toUpperCase();
-  if (isNaN(numberOfDogsInMyHouse)){
+  if (isNaN(numberOfDogsInMyHouse)) {
     alert('enter a number!');
-  }else if (numberOfDogsInMyHouse > 3){
+  } else if (numberOfDogsInMyHouse > 3) {
     alert('try again, ' + userName + ' try again')
-  }else if(numberOfDogsInMyHouse < 2){
+  } else if (numberOfDogsInMyHouse < 2) {
     alert('again, ' + userName + ', try again')
-  }else if ('correct, ' + numberOfDogsInMyHouse === 2){
-    correctAnswer++;
-  break
-  } else if (numberOfDogsInMyHouse === 6){
+  } else if (numberOfDogsInMyHouse === '2') {
+    attempts = 6;
+  } else if (attempts === 6) {
     alert('You are out guesses, ' + userName + '.  Click okay to finish.');
   }
 }
-  
+
 
 
 
@@ -123,4 +122,3 @@ alert('Thanks for playing ' + userName + '!');
 // Thanks for playing userName!
 //alert(`Hi ${userName}, this is template literal!`);
 
- 
